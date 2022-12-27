e4f104ee9891881f9629b08805f5dd50925639c0
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2022-12-27 12:18:41 -0300

    Condicional IF

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 60.5
+
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if(vel>60){
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+
+console.log(`Dirija se,pre usando o sinto de segurança!`)
+
+
---
+++ javascript/aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'EUA'
+
+console.log(`Vivendo em ${pais}`)
+if( pais == 'BR'){
+    console.log(`Você é brasileiro!`)
+}
+else{
+    console.log(`Você é estrangeiro!`)
+}
