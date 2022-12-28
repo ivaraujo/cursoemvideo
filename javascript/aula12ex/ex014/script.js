eef664e0ab1e455aee0aa64e90715a04e5f648b5
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2022-12-28 10:30:40 -0300

    exercicio de IF01

---
+++ javascript/aula12ex/ex014/script.js
@@ -0,0 +1,27 @@
+
+
+function carregar(){
+    var msg = window.document.getElementById('msg');
+    var img = window.document.getElementById('imagem');
+    var data = new Date();
+    var hora = data.getHours();
+    
+    msg.innerHTML = `Agora são ${hora} horas.`;
+
+    if(hora >= 0 && hora < 12){
+        //BOMDIA
+        img.src = 'manha.png';
+        document.body.style.background = '#e2cd9f'
+    }
+    else if(hora<=18){
+        //BOATARDE
+        img.src = 'tarde.png';
+        window.document.body.style.background = '#b9846f'
+    }
+    else{
+        //BOANOITE
+        img.src = 'noite.png';
+        document.body.style.background = '#515154'
+    }
+
+}
