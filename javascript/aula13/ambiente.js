d94ab5d83cd84617921e2fec27e6d23a7f775336
Ivan Viana <82772539+ivaraujo@users.noreply.github.com>
2022-12-30 11:08:25 -0300

    Aula de While

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,19 @@
+/*console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
+
+var c = 0
+
+/*
+while(c < 6){
+    c++
+    console.log(`Passo: ${c}`)
+} */
+
+do{
+    c++
+    console.log(`Passo: ${c}.`)
+}while(c < 6)
